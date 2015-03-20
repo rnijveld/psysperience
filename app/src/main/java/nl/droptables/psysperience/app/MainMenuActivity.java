8c0466861d821b25e60b47db6f0883ae519db99f
Sjors Gielen <sjors@sjorsgielen.nl>
2015-03-20 19:39:27 +0100

    Add VideoActivity, which reads images from the camera and displays them.

--- app/src/main/java/nl/droptables/psysperience/app/MainMenuActivity.java
+++ app/src/main/java/nl/droptables/psysperience/app/MainMenuActivity.java
@@ -1,9 +1,12 @@
 package nl.droptables.psysperience.app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainMenuActivity extends Activity {
@@ -12,6 +15,15 @@ public class MainMenuActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        final Activity thisActivity = this;
+
+        final Button button = (Button) findViewById(R.id.start);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                final Intent intent = new Intent(thisActivity, VideoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
